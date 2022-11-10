2043109a98319b0470394dc22f538a0cd8f605d6
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-11-09 17:02:34 -0800

    Create a switchTurns function and begin psuedocoding for checkForWinner function

--- main.js
+++ main.js
@@ -5,5 +5,26 @@ var playerTwo = newGame.createPlayers('playerTwo');
 newGame.turn = playerOne.name;
 console.log(newGame);
 
-newGame.playerOneMoves.push(2);
-newGame.playerTwoMoves.push(5);
+
+function playerMove(player, move) {
+    player.playerMove(move);
+    newGame.playerMove(player, move);
+}
+
+function switchTurns() {
+    if (newGame.turn === playerOne) {
+        newGame.turn = playerTwo;
+    } else if (newGame.turn === playerTwo) {
+        newGame.turn = playerOne;
+    }
+}
+
+// create a for loop that will check the moves in the array
+// I think it makes more sense to check in the players array 
+// it can be more dynamic.
+// If the numbers in each players array is 
+
+
+function checkForWinner() {
+    for 
+}
--- player.js
+++ player.js
@@ -2,8 +2,12 @@ class Player {
     constructor(playerName) {
         this.name = playerName
         this.wins = 0;
+        this.moves = [];
     }
     playerWon() {
         this.wins ++;
+    }
+    playerMove(move) {
+        this.moves.push(move);
     }
 }
